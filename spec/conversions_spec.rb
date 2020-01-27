12a08d1e8c30814264398eee8c530b6e395acc8f
Fernando Lopez <fernando_lopezc@live.com>
2020-01-27 22:27:35 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -2,7 +2,10 @@ puts "Tests are running..."
 
 describe "conversions" do
   describe "#ounces_to_grams" do
-    it "given 0, returns 0.0"
-  end
-
-end
+    it "given 0, returns 0.0" do
+      grams = ounces_to_grams(0)
+      expect(grams).to eq(0.0)
+    end
+  end 
+end    
+    
